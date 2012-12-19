161675edab68c18b38668c03097fdb91019f7974
Chunikhin Denis <chunikhin.denis@gmai.com>
2012-12-19 16:23:39 +0200

    Update Parser.rb

--- Parser.rb
+++ Parser.rb
@@ -8,30 +8,33 @@ class Grabber
 	end
 	def search
 		mas=Array.new
-		a=Array.new
 		mas.push @url
 		def func(n,mas)
 		if n>0 
+			a=Array.new
 			for i in 0..mas.size-1
 				begin
-				p = Nokogiri::HTML(open(mas[i]))
-				resque Errno::ENOENT
+					p = Nokogiri::HTML(open(mas[i]))
+				rescue Errno::ENOENT
 					mas.delete_at(i)
 					next
 				end	
 				p.css('a').each do |link|
-					if link['href'][0]=='#'
-						next
-					elsif link['href'][0]=='/'
-						a.push mas[i]+link['href']
-					else	
-						a.push link['href']
+					if link['href']!=nil
+						if link['href'][0]=='#'
+							next
+						elsif (link['href'][0]=='/')
+							a.push mas[i]+link['href']
+						else	
+							a.push link['href']
+						end	
 					end	
 				end	
 				a.uniq!	
 				a.unshift mas[i]
 				mas[i]=a
-				func(n-1,mas)
+				#puts mas[i]
+				func(n-1,mas[i])
 			end	
 		end	
 		end
@@ -45,5 +48,5 @@ class Grabber
 
 end
 
-g=Grabber.new("http://sheldonbrown.com/web_sample1.html#h",2)
+g=Grabber.new("http://google.com",2)
 g.search
